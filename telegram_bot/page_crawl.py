f5323fa1d17875d40e169c0fc59c3b9a2483dc88
paulham98 <pauljungho.ham@gmail.com>
2022-09-14 13:17:09 +0900

    건물 타입

--- telegram_bot/page_crawl.py
+++ telegram_bot/page_crawl.py
@@ -22,6 +22,14 @@ def get_estate_url(area):
 
 
 def get_estate_crawl(source):
+    """
+    #rletTypeCd: A01=아파트, A02=오피스텔, B01=분양권, 주택=C03, 토지=E03, 원룸=C01, 상가=D02, 사무실=D01, 공장=E02, 재개발=F01, 건물=D03
+    # tradeTypeCd (거래종류): all=전체, A1=매매, B1=전세, B2=월세, B3=단기임대
+    # hscpTypeCd (매물종류): 아파트=A01, 주상복합=A03, 재건축=A04 (복수 선택 가능)
+    # cortarNo(법정동코드): (예: 1168010600 서울시, 강남구, 대치동)
+    :param source:
+    :return:
+    """
     url = f"https://m.land.naver.com{source}"
     # url = "https://m.land.naver.com/complex/info/453?ptpNo=1"
     header = {
