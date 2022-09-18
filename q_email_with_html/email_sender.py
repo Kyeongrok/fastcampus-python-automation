efff3a95b92896c87eb46a1fc14d7cdef6e94d64
Kyeongrok <oceanfog1@gmail.com>
2022-09-18 12:59:02 +0900

    attach files

--- q_email_with_html/email_sender.py
+++ q_email_with_html/email_sender.py
@@ -2,11 +2,13 @@ import os
 import smtplib
 from email.encoders import encode_base64
 from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formataddr
 from pathlib import Path
 
 from openpyxl.reader.excel import load_workbook
+from datetime import datetime
 
 
 class EmailSender:
@@ -18,33 +20,32 @@ class EmailSender:
         'naver.com':'smtp.naver.com'
     }
     smtp_server = None
-    attachments_path = None
+    template_filename = None
 
-    def __init__(self, email_addr, password, manager_name, attachments_path='data/'):
+    def __init__(self, email_addr, password, manager_name, template_filename, attachment_path='data/'):
         print('생성자')
         self.email_addr = email_addr
         self.manager_name = manager_name
+        if password == None or '':
+            raise Exception('password를 입력해주세요.')
         self.password = password
         self.smtp_server = self.smtp_server_map[email_addr.split('@')[1]] # fc.krkim@gmail.com
-        self.attachments_path = attachments_path
         print(self.smtp_server)
+        self.template_filename = template_filename
+        self.attachments_path = attachment_path
 
-    def send_email(self, msg, from_addr, to_addr, receiver_name, subject, attachment):
-        """
-        :param msg: 보낼 메세지
-        :param from_addr: 보내는 사람
-        :param to_addr: 받는 사람
-        :return:
-        """
+    def send_email(self, html_msg, from_addr, to_addr, receiver_name, subject, attachment):
+
         with smtplib.SMTP(self.smtp_server, 587) as smtp:
-            msg = MIMEText(msg)
+            msg = MIMEMultipart('alternative')
             msg['From'] = formataddr((self.manager_name, from_addr))
             msg['To'] = formataddr((receiver_name, to_addr))
-            msg['Subject'] = subject
-            # 파일 첨부
+            msg['Subject'] = subject + str(datetime.now())
+            msg.attach(MIMEText(html_msg, 'html', 'utf-8'))
             if attachment:
                 part = MIMEBase('application', "octet-steam")
                 filenm = Path(attachment).name
+                print('filenm:', filenm)
                 with open(self.attachments_path + attachment, 'rb') as f:
                     part.set_payload(f.read())
                     encode_base64(part)
@@ -63,27 +64,20 @@ class EmailSender:
         ws = wb.active
 
         for row in ws.iter_rows(min_row=2):
-            temp1 = """
-안녕하세요 %받는분%님 패스트몰 %매니저_이름% 입니다.
-
-귀사에 무궁한 발전을 기원 합니다.
-금일 쇼핑몰로 주문 들어온 주문건들을 보내드립니다.
-확인 해보시고 발주 부탁드립니다.
-패스트몰 %매니저_이름% 드림
-            """
-
             if row[0].value != None:
-                print(row[0].value, row[1].value, row[2].value)
-                temp1 = temp1.replace('%받는분%', row[1].value)
-                temp1 = temp1.replace('%매니저_이름%', self.manager_name)
-                self.send_email(msg=temp1,
-                              from_addr=self.email_addr,
-                              to_addr=row[0].value, receiver_name=row[1].value,
-                              subject=row[2].value, attachment='[패스트몰] 그레이스개러지.xlsx')
+                with open(self.template_filename, encoding='utf-8') as f:
+                    temp1 = f.read()
+                    print(row[0].value, row[1].value, row[2].value)
+                    temp1 = temp1.replace('%받는분%', row[1].value)
+                    temp1 = temp1.replace('%매니저_이름%', self.manager_name)
+                    self.send_email(html_msg=temp1,
+                                  from_addr=self.email_addr,
+                                  to_addr=row[0].value, receiver_name=row[1].value,
+                                  subject=row[2].value, attachment=row[3].value)
+            else:
+                print('row[0]이 None입니다.')
 
 if __name__ == '__main__':
     # es = EmailSender('fc.krkim@gmail.com', os.getenv('MY_GMAIL_PASSWORD'))
     es = EmailSender('oceanfog@naver.com', os.getenv('MY_NAVER_PASSWORD'), manager_name='김미령')
-    es.send_all_emails('이메일리스트_with_attachment.xlsx')
-
-
+    es.send_all_emails('이메일리스트_with_name.xlsx')
